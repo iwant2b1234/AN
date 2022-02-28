7cf57b32c12c2673a930c37ca6c29b1c6c09745e
iwant2b1234 <iwant2b1234@gmail.com>
2022-02-28 22:19:57 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ const path = require('path');
 module.exports = {
     publicPath: './',
     // productionSourceMap:process.env.VUE_APP_BASIC_TYPE=='dev'?true:false,
-    outputDir: 'docs',
+    outputDir: 'dist',
     css: {
       loaderOptions: {
         // sass: {
